f09d97b4037aff96151e777f23c3aa866310096a
Mustafa Emir Öziri <mustafa.emir761@gmail.com>
2023-07-14 23:05:47 +0300

    Pressure Pad and Door Opening
    Created a pressure pad prefab with a script. It opens the door when activated.

---
+++ Assets/Scripts/CubeActivate.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeActivate : MonoBehaviour
+{
+    
+    public static int cubeCount = 0;
+    public static bool doorOpen = false;
+    public static int hedefSayi = 2;
+    public GameObject door;
+    private Vector3 oldLocation;
+    private Vector3 newLocation;
+
+    void Start()
+    {
+        oldLocation = door.gameObject.transform.position;
+        newLocation = new Vector3(oldLocation.x + 6f, oldLocation.y, oldLocation.z + 5.5f);
+    }
+
+    void Update()
+    {
+        if ((cubeCount == hedefSayi) && (doorOpen == false))
+        {
+            //Debug.Log("kapi acildi");
+            doorOpen = true;
+            OpenDoor();
+        }
+
+        else if ((cubeCount != hedefSayi) && (doorOpen == true))
+        {
+            //Debug.Log("kapi kapandi");
+            doorOpen = false;
+            CloseDoor();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "cube")
+        {
+            //Debug.Log("kup girdi");
+            cubeCount++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "cube")
+        {
+            //Debug.Log("kup cikti");
+            cubeCount--;
+        }
+    }
+
+    private void OpenDoor()
+    {
+        door.gameObject.transform.position = newLocation;
+    }
+
+    private void CloseDoor()
+    {
+        door.gameObject.transform.position = oldLocation;
+    }
+}
